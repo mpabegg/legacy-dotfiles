5066428645b881f68a9e7159106e076b610140bf
Serge Gebhardt <serge.gebhardt@gmail.com>
2013-02-17 02:34:54 -0300

    zsh: add zle bindkeys

--- zsh/custom/plugins-common/zle.zsh
+++ zsh/custom/plugins-common/zle.zsh
@@ -1,9 +1,37 @@
-insert_sudo () {
-    zle beginning-of-line
-    zle -U ""
-    zle -U "sudo "
-}
+# keybindings and other goodness
 
-zle -N insert-sudo insert_sudo
-bindkey "^[s" insert-sudo
+# alt+s to prepend 'sudo ' to current command and move to EOL
+bindkey -s '\es' '^Asudo ^E'
 
+# alt+u to move to parent directory and list it
+bindkey -s '\eu' '^Ucd ..; ls^M'
+
+# alt+U to move to parent directory
+bindkey -s '\eU' '^Ucd ..^M'
+
+# If AUTO_PUSHD is set, alt+p pops the dir stack
+bindkey -s '\ep' '^Upopd >/dev/null; dirs -v^M'
+
+# alt+F: find files matching patterns using fastfind (see other zsh file)
+bindkey -s '\eF' '^Aff ^M'
+
+# alt+D: find directories matching patterns using fastfind (see other zsh file)
+bindkey -s '\eD' '^Afd ^M'
+
+# alt+V: open the given fastfind result in vim
+bindkey -s '\eV' '^Afvi ^M'
+
+# alt+l: execute 'ls'
+bindkey -s '\el' '^Uls^M'
+
+# alt+L: execute 'ls -l'
+bindkey -s '\eL' '^Uls -l^M'
+
+# alt+v: pipe current command to view
+bindkey -s '\ev' '^E 2>&1 | view -^M'
+
+# alt+g: execute 'git status'
+bindkey -s '\eg' '^Ugit status^M'
+
+# alt+G: execute 'git status .'
+bindkey -s '\eG' '^Ugit status .^M'
