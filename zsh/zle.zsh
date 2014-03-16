972116c259ebd983a5e4440b7fbe6120552175e7
Serge Gebhardt <serge.gebhardt@gmail.com>
2014-03-16 13:45:44 -0300

    zsh: alt+d for `pwd` and minor changes to ZLE

--- zsh/zle.zsh
+++ zsh/zle.zsh
@@ -3,6 +3,9 @@
 # alt+s to prepend 'sudo ' to current command and move to EOL
 bindkey -s '\es' '^Asudo ^E'
 
+# alt+d to show current working directory
+bindkey -s '\ed' '^Upwd^M'
+
 # alt+u to move to parent directory and list it
 bindkey -s '\eu' '^Ucd ..; ls^M'
 
@@ -18,8 +21,8 @@ bindkey -s '\eP' '^Udirs -v^M'
 # alt+l: execute 'ls'
 bindkey -s '\el' '^Uls^M'
 
-# alt+L: execute 'ls -lA'
-bindkey -s '\eL' '^Uls -lA^M'
+# alt+L: execute 'ls -lhA'
+bindkey -s '\eL' '^Uls -lhA^M'
 
 # alt+v: pipe current command to view
 bindkey -s '\ev' '^E 2>&1 | view -^M'
@@ -30,7 +33,7 @@ bindkey -s '\eg' '^Ugws^M'
 # alt+G: execute 'git status .'
 bindkey -s '\eG' '^Ugws .^M'
 
-# alt+j: add new-line for multi-line edits in zle (needs exception for midnight commander)
+# control+j: add new-line for multi-line edits in zle (needs exception for midnight commander)
 if [[ "$MC_SID" != "" || "$MC_CONTROL_PID" != "" ]]; then
     bindkey "^J" accept-line
 else
@@ -39,4 +42,3 @@ fi
 
 # alt+.: insert last argument of previous command
 bindkey '\e.' insert-last-word
-
