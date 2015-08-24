6d450c2ee34fb14308e2704fb8ff2b5c54cafed7
Matheus Abegg <matheus.abegg@gmail.com>
2015-08-24 16:20:07 -0300

    install homebrew and homebrew cask

---
+++ homebrew/install.sh
@@ -0,0 +1,8 @@
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew install caskroom/cask/brew-cask
+exit 0
