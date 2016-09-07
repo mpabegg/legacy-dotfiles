d699e52124a946530ce28093e526f77d8928bffb
Matheus Abegg <matheus.abegg@gmail.com>
2016-09-07 18:43:44 -0400

    removes brewcask installation

--- setup.sh
+++ setup.sh
@@ -123,8 +123,6 @@ install_homebrew () {
       echo "  Installing Homebrew for you."
       ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
-
-    brew install caskroom/cask/brew-cask
   fi
 }
 
